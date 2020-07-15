4f84e8d7bff5e4f4fc26e2bd9264ec9fd992943e
Jakkins <36565273+Jakkins@users.noreply.github.com>
2020-07-15 19:47:38 +0000

    Update conf.sh

--- Script/conf.sh
+++ Script/conf.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-time { ../sources/binutils-2.34/configure		\
-				--prefix=/lfs/tools 			\
-				--with-sysroot=/lfs				\
+time { ../sources/binutils-2.34/configure			\
+				--prefix=/lfs/tools 		\
+				--with-sysroot=/lfs		\
 				--with-lib-path=/lfs/tools/lib 	\
-				--target=$LFS_TGT          		\
-				--disable-nls              		\
-				--disable-werror				\
+				--target=$LFS_TGT          	\
+				--disable-nls              	\
+				--disable-werror		\
  && make && make install; }
